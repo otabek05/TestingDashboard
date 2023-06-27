e6e854e8a74a33dafb974b637673ade11e536de9
otabek05 <otabektoshmukhammedov@gmail.com>
2023-06-27 12:07:10 +0900

    Initial Commit

---
+++ src/assets/dummy-data/navLinks.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    path: "/dashboard",
+    icon: "ri-apps-2-line",
+    display: "Dashboard",
+  },
+  {
+    path: "/bookings",
+    icon: "ri-taxi-line",
+    display: "Employees",
+  },
+  {
+    path: "/sell-car",
+    icon: "ri-shopping-bag-line",
+    display: "Buildings",
+  },
+  {
+    path: "/settings",
+    icon: "ri-settings-2-line",
+    display: "Settings",
+  },
+];
+
+export default navLinks;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import "../styles/dashboard.css";
+import SingleCard from "../components/reuseable/SingleCard";
+
+import MileChart from "../charts/MileChart";
+import CarStatsChart from "../charts/CarStatsChart";
+import RecommendCarCard from "../components/UI/RecommendCarCard";
+
+import recommendCarsData from "../assets/dummy-data/recommendCars";
+import MapComponent from "../components/MapComponent";
+
+const carObj = {
+  title: "Total Buildings",
+  totalNumber: 750,
+  icon: "ri-police-car-line",
+};
+
+const tripObj = {
+  title: "Total Sensors",
+  totalNumber: 1697,
+  icon: "ri-steering-2-line",
+};
+
+const clientObj = {
+  title: "Total Clients",
+  totalNumber: "85k",
+  icon: "ri-user-line",
+};
+
+const distanceObj = {
+  title: "Total Employees",
+  totalNumber: 2167,
+  icon: "ri-timer-flash-line",
+};
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="dashboard__wrapper">
+        <div className="dashboard__cards">
+          <SingleCard item={carObj} />
+          <SingleCard item={tripObj} />
+          <SingleCard item={clientObj} />
+          <SingleCard item={distanceObj} />
+        </div>
+      </div>
+      
+      <MapComponent />
+    </div>
+  );
+};
+
+export default Dashboard;
